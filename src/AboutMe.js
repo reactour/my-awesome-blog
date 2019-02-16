466081552f6c58314197fe253fc79902a49728f1
Johannes Elmnäinen <sanhologin@gmail.com>
2019-02-16 20:41:07 +0200

    Checkpoint: introducing variables

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,12 +2,16 @@ import React, { Component }  from 'react';
 
 class AboutMe extends Component {
   render () {
+    let name = "Johannes"
+    let age = 31
+    let hobbies = "ultimate frisbee, computer games"
+
     return (
       <div className="about-card">
-        <h3>Johannes</h3>
+        <h3>{name}</h3>
         <ul>
-          <li>Age: 30</li>
-          <li>Hobbies: ultimate frisbee, computer games</li>
+          <li>Age: {age}</li>
+          <li>Hobbies: {hobbies}</li>
         </ul>
       </div>
     )
