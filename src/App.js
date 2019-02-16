86acd75320061162cee83a15c63f3c58f4519536
Johannes Elmnäinen <sanhologin@gmail.com>
2019-02-16 14:16:35 +0200

    Situation after HTML challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="masthead">My awesome blog</h1>
+        <div className="about">
+          <h2>About</h2>
+          <div className="about-cards">
+            <div className="about-card">
+              <h3>Johannes</h3>
+              <ul>
+                <li>Age: 30</li>
+                <li>Hobbies: ultimate frisbee, computer games</li>
+              </ul>
+            </div>
+            <div className="about-card">
+              <h3>Pauli</h3>
+              <ul>
+                <li>Age: 30</li>
+                <li>Hobbies: computer games, board games, dancing</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
