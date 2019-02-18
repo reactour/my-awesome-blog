7d61b1075d385b0bb601b796c20ab4958d7d9ffd
Johannes Elmnäinen <sanhologin@gmail.com>
2019-02-18 18:09:43 +0200

    Checkpoint: after functions-chapter challenge

--- src/AboutCard.js
+++ src/AboutCard.js
@@ -1,6 +1,18 @@
 import React, { Component }  from 'react';
 
 class AboutCard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      likes: 0
+    }
+  }
+
+  modifyLikes(amount) {
+    let newLikes = this.state.likes + amount
+    this.setState({likes: newLikes})
+  }
+
   render () {
     return (
       <div className="about-card">
@@ -9,6 +21,17 @@ class AboutCard extends Component {
           <li>Age: {this.props.age}</li>
           <li>Hobbies: {this.props.hobbies}</li>
         </ul>
+        <div className="likes-wrapper">
+          <div className="likes">
+            {this.state.likes}
+          </div>
+          <button className="btn" onClick={() => this.modifyLikes(1)}>
+            +
+          </button>
+          <button className="btn" onClick={() => this.modifyLikes(-1)}>
+            -
+          </button>
+        </div>
       </div>
     )
   }
