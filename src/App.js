4620ce60bb55e78bad1ae682dd59d40f194014a0
Johannes Elmnäinen <sanhologin@gmail.com>
2019-02-19 16:15:02 +0200

    Maps and Loops, before lifting likes to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,34 @@ import AboutCard from './AboutCard'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      writers: [
+        {
+          name: "Johannes",
+          age: 30,
+          hobbies: "ultimate frisbee, computer games"
+        },
+        {
+          name: "Pauli",
+          age: 30,
+          hobbies: "board games, dancing"
+        }
+      ]
+    }
+  }
+
+  renderAboutCard(writer) {
+    return(
+      <AboutCard
+        name={writer.name}
+        age={writer.age}
+        hobbies={writer.hobbies}
+      />
+    )
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,16 +39,7 @@ class App extends Component {
         <div className="about">
           <h2>About</h2>
           <div className="about-cards">
-            <AboutCard
-              name="Johannes"
-              age={30}
-              hobbies="ultimate frisbee, computer games"
-            />
-            <AboutCard
-              name="Pauli"
-              age={30}
-              hobbies="board games, dancing"
-             />
+            {this.state.writers.map(this.renderAboutCard)}
           </div>
         </div>
       </div>
